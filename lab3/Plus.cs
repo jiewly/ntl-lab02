d66205ef911a62330d60fe5e30c61a3d61864537
Irin Krapookthong (NTL) <CF303520@tidlor.com>
2021-07-07 10:56:40 +0700

    init

---
+++ lab3/Plus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class Plus
+    {
+        public double num1 = 0.0; //properties
+        public double num2 = 0.0;
+        public Plus(double num1, double num2)//constructor
+        {
+            this.num1 = num1;//Dependency
+            this.num2 = num2;//Dependency
+        }
+        public double Shownumber1()
+        {
+            return this.num1;
+        }
+        public double Shownumber2()
+        {
+            return this.num2;
+        }
+        public double summary()
+        {
+            return this.num1 + this.num2;
+
+
+        }
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input number A+B");
+            Console.WriteLine("-----------------------");
+
+            Console.Write("in put number A=");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("in put number B=");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            var plu = new Plus(num1, num2);
+
+            var summary = plu.summary();
+            Console.WriteLine("Summary=" + summary);
+            Console.ReadLine();
+
+
+            {
+
+            }
+        }
+    }
+}
